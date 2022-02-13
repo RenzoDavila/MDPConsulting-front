e59b67a88260663fe4d03427cd97a68a50179526
Renzo Davila <renzo.davila@tecsup.edu.pe>
2022-02-13 04:05:57 -0500

    Updates

---
+++ precache-manifest.6761c16f1bf252d6531a54514259faf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00513b9d66ad07bacdf947bff40ab5a",
+    "url": "/MDPConsulting-front/index.html"
+  },
+  {
+    "revision": "533ee8a373811eb9593b",
+    "url": "/MDPConsulting-front/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8706ae71047cf84784b8",
+    "url": "/MDPConsulting-front/static/css/main.0af227c7.chunk.css"
+  },
+  {
+    "revision": "533ee8a373811eb9593b",
+    "url": "/MDPConsulting-front/static/js/2.9914627c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MDPConsulting-front/static/js/2.9914627c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8706ae71047cf84784b8",
+    "url": "/MDPConsulting-front/static/js/main.cac115f3.chunk.js"
+  },
+  {
+    "revision": "a309827c6a6cd14426f5",
+    "url": "/MDPConsulting-front/static/js/runtime-main.afecc77f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MDPConsulting-front/static/media/logo.5d5d9eef.svg"
+  }
+]);
